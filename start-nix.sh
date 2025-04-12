d00450e44b2c05d7a704e02cd603f004fe510fcf
Dralexgon <99817451+Dralexgon@users.noreply.github.com>
2025-04-12 23:13:35 +0200

    saved opengl

--- start-nix.sh
+++ start-nix.sh
@@ -1,3 +1,4 @@
 nix develop --command bash -c "python src/main.py"
-#nix develop --command bash -c "python src/test_opengl.py"
+#nix develop --command bash -c "python src/triangle_opengl.py"
+#nix develop --command bash -c "python src/change_color_opengl.py"
 #nix develop --command bash -c "python src/main_cairo.py"
